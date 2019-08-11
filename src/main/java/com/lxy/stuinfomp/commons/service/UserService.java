9baeeaecb2915a6062ad4e6f955230e1a074a76f
robin_time <robin_time@163.com>
2019-08-12 00:34:00 +0800

    登录优化

--- src/main/java/com/lxy/stuinfomp/commons/service/UserService.java
+++ src/main/java/com/lxy/stuinfomp/commons/service/UserService.java
@@ -6,4 +6,6 @@ import com.lxy.stuinfomp.commons.domain.Users;
  * @author lxy
  */
 public interface UserService extends BaseCrudService<Users> {
+
+    Users selectByUserName(String userName);
 }
--- src/main/java/com/lxy/stuinfomp/commons/service/impl/UserServiceImpl.java
+++ src/main/java/com/lxy/stuinfomp/commons/service/impl/UserServiceImpl.java
@@ -5,9 +5,20 @@ import com.lxy.stuinfomp.commons.mapper.UsersMapper;
 import com.lxy.stuinfomp.commons.service.UserService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 /**
  * @author lxy
  */
 @Service
 public class UserServiceImpl extends BaseCrudServiceImpl<Users,UsersMapper> implements UserService {
+
+    @Resource
+    private UsersMapper usersMapper;
+
+
+    @Override
+    public Users selectByUserName(String userName) {
+        return usersMapper.selectByUserName(userName);
+    }
 }
