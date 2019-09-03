0a679d5a809cb40bfc3d8af42b0a8f1ddf7269de
robin_time <robin_time@163.com>
2019-09-04 00:43:22 +0800

    crud 接口测试

--- src/main/java/com/lxy/stuinfomp/commons/service/CourseService.java
+++ src/main/java/com/lxy/stuinfomp/commons/service/CourseService.java
@@ -1,10 +1,16 @@
 package com.lxy.stuinfomp.commons.service;
 
 import com.lxy.stuinfomp.commons.domain.Courses;
-import com.lxy.stuinfomp.commons.service.impl.BaseCrudServiceImpl;
 
 /**
  * @author lxy
  */
 public interface CourseService extends BaseCrudService<Courses> {
+    /**
+     * 查询最大id值
+     * @return
+     */
+    default Long selectMaxId(){
+        return null;
+    }
 }
--- src/main/java/com/lxy/stuinfomp/commons/service/impl/CourseServiceImpl.java
+++ src/main/java/com/lxy/stuinfomp/commons/service/impl/CourseServiceImpl.java
@@ -3,6 +3,7 @@ package com.lxy.stuinfomp.commons.service.impl;
 import com.lxy.stuinfomp.commons.domain.Courses;
 import com.lxy.stuinfomp.commons.mapper.CoursesMapper;
 import com.lxy.stuinfomp.commons.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,4 +11,12 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CourseServiceImpl extends BaseCrudServiceImpl<Courses, CoursesMapper> implements CourseService {
+
+    @Autowired
+    private CoursesMapper coursesMapper;
+
+    @Override
+    public Long selectMaxId() {
+        return coursesMapper.selectMaxId();
+    }
 }
