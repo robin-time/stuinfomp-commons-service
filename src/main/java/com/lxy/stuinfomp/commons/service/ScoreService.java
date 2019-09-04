a4461afc28ef1fb577571d2fe3bf52df149f9ea9
robin_time <robin_time@163.com>
2019-09-04 22:59:35 +0800

    score crud 接口测试

---
+++ src/main/java/com/lxy/stuinfomp/commons/service/ScoreService.java
@@ -0,0 +1,6 @@
+package com.lxy.stuinfomp.commons.service;
+
+import com.lxy.stuinfomp.commons.domain.Score;
+
+public interface ScoreService extends BaseCrudService<Score> {
+}
---
+++ src/main/java/com/lxy/stuinfomp/commons/service/impl/ScoreServiceImpl.java
@@ -0,0 +1,11 @@
+package com.lxy.stuinfomp.commons.service.impl;
+
+import com.lxy.stuinfomp.commons.domain.Score;
+import com.lxy.stuinfomp.commons.mapper.ScoreMapper;
+import com.lxy.stuinfomp.commons.service.ScoreService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScoreServiceImpl extends BaseCrudServiceImpl<Score, ScoreMapper> implements ScoreService {
+
+}
